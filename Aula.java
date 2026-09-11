4d028eef5cab24d811b09670959c742b5575d089
Eduard341 <s.eduard@escola.pr.gov.br>
2026-09-11 12:46:03 +0000

    Class.java

---
+++ Aula.java
@@ -0,0 +1,10 @@
+public class Aula {
+    public static void main(String[] args){
+        carrinho.add("Monitor");
+
+        System.out.println(carrinho);
+        System.out.println("O ultimo produto e : " + carrinho.get(5));
+        
+    }
+
+}
